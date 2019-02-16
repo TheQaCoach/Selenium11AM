d3da1ab15e8bd28e53cb478284f93537f00eca82
rohbar13 <rohbar13@gmail.com>
2019-02-16 13:51:50 +0530

    Changes made to find the largest and smallest number

--- src/controlflow/questions/SmallestLargest.java
+++ src/controlflow/questions/SmallestLargest.java
@@ -1,23 +1,25 @@
 package controlflow.questions;
 
 public class SmallestLargest {
-
 	public static void main(String arg[]) {
-
 		int arr[] = { 10, 3, 30, 45, 20, 98, 65, 24 };
-
-		// Finding the smallest number
 		int minValue = 0;
-		
-		for (int x = 0; x < arr.length - 1; x++) {
-			int temp = minValue;
-			for(int y = 0; y < arr.length; y++) {
-				if(arr[x] < arr[y]) {
-					minValue = arr[x];
-				}
+		minValue = arr[0];
+		for(int x=0;x<arr.length;x++) {
+			if(arr[x]<minValue) {
+				minValue = arr[x];
 			}
 		}
-		
 		System.out.println(minValue);
+		
+		
+		int maxValue = 0;
+		maxValue = arr[0];
+		for(int x=0;x<arr.length;x++) {
+			if(arr[x]>maxValue) {
+				maxValue = arr[x];
+			}
+		}
+		System.out.println(maxValue);
 	}
 }
